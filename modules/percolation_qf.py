584873c2143a7ddd0d64fab039927f28c5701b69
phenibutbarriers <igorukhov2015@yandex.ru>
2023-04-15 22:47:23 +0300

    percolation_qf start + fix by likstorovi4

--- modules/percolation_qf.py
+++ modules/percolation_qf.py
@@ -1,11 +1,15 @@
 class Percolation:
     def __init__(self, N):
-        self.   matrix = [[(False)] for x in range(N)]
+        self.matrix = [[(False)] for x in range(N)]
+
     def open(self, i: int, j: int) -> None:
         self.matrix[i][j] = True
+
     def isOpen(self, i: int, j: int) -> bool:
         return self.matrix[i][j]
+
     def isFull(self, i: int, j: int) -> bool:
         return self.matrix[i][j]
+
     def percolates(self):
         ...
