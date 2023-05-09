3014c875df68b91546931db031ced68515c8164d
Likstor <94634271+Likstor@users.noreply.github.com>
2023-05-09 22:53:12 +0300

    method open ver 1

--- modules/percolation_qu.py
+++ modules/percolation_qu.py
@@ -1,19 +1,92 @@
+class UnionFind:
+    def __init__(self, size: int = None) -> None:
+        self._id = [] if size is None else list(range(size ** 2))
+        self._size = [] if size is None else [1] * (size ** 2)
+
+    def make_set(self) -> None:
+        self._id.append(len(self._id))
+        self._size.append(1)
+
+    def find_set(self, x: int) -> int:
+        return self._id[x]
+
+    def __str__(self) -> str:
+        str_1 = ' '.join(map(str, self._id))
+        str_2 = ' '.join(map(str, self._size))
+        return str_1 + '\n' + str_2
+
+    def root(self, x: int) -> int:
+        if x == self._id[x]:
+            return x
+
+        self._id[x] = self.root(self._id[x])
+        return self._id[x]
+
+    def union_set(self, x: int, y: int) -> None:
+        root_x = self.root(x)
+        root_y = self.root(y)
+
+        if root_x == root_y:
+            return
+
+        if self._size[root_x] < self._size[root_y]:
+            self._id[root_x] = root_y
+            self._size[root_y] += self._size[root_x]
+            return
+
+        self._id[root_y] = root_x
+        self._size[root_x] += self._size[root_y]
+
+    def connected(self, x: int, y: int) -> bool:
+        return self.root(x) == self.root(y)
+
+
 class Percolation:
     def __init__(self, size: int) -> None:
-        self.__matrix = [[False] * size for _ in range(size)]
-        
+        self._matrix = [[False] * size for _ in range(size)]
+        self._uf = UnionFind(size)
+
+    def __str__(self):
+        output = '    ' + \
+            '     '.join(map(str, range(len(self._matrix)))) + '\n'
+        idx = 0
+
+        while True:
+            output += f'{idx} ' + ' '.join(map(lambda x: f'{x} ' if x is True else str(x), self._matrix[idx]))
+            idx += 1
+
+            if idx < len(self._matrix):
+                output += '\n'
+                continue
+
+            break
+
+        return output
+
     @property
     def matrix(self) -> list:
-        return self.__matrix
-        
+        return self._matrix
+    
+    def isOpen(self, string: int, column: int) -> bool:
+        return self._matrix[string][column]
+
     def open(self, string: int, column: int) -> None:
         self._matrix[string][column] = True
         
-    def isOpen(self, string: int, column: int) -> bool:
-        return self._matrix[string][column]
+        if column - 1 > -1 and self._matrix.isOpen(string, column - 1):
+            self._uf.union_set(column - 1 + string * len(self._matrix)) 
         
+        if column + 1 < len(self._matrix) and self._matrix.isOpen(string, column + 1):
+            self._uf.union_set(column + 1 + string * len(self._matrix))
+        
+        if string - 1 > -1 and self._matrix.isOpen(string - 1, column):
+            self._uf.union_set(column + (string - 1) * len(self._matrix))
+            
+        if string + 1 < len(self._matrix) and self._matrix.isOpen(string + 1, column):
+            self._uf.union_set(column + (string + 1) * len(self._matrix))
+
     def isFull(self, string: int, column: int) -> bool:
-        return self._matrix[string][column]
-        
+        ...
+
     def percolates(self) -> bool:
         ...
